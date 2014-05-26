4ac8cdaa91cb544d6e0e1eed69a56292119ecac3
Jay Wilner <info@outofboundscommunications.com>
2014-05-26 13:23:00 -0500

    working copy except for stop button

--- lesson9/imageShow3.js
+++ lesson9/imageShow3.js
@@ -8,17 +8,25 @@ var $rotator = $("#images");
 
 //select button 
 var $button = $("input:button");
+console.log($button);
 
 //select button for stopping animation
 var $stopButton = $("input:button[value='stop']");
+console.log($stopButton);
 
 //select button for restarting animation
 //var $startButton = $button.attr( "value", "start" );
 var $startButton = $("input:button[value='start']");
+console.log($startButton);
 
 //define click function on clicking start button
+//user clicks this start button to start the rotation of images
+//this function also changes the label of that button to 'stop'
+//so user can then click again to stop the animation/rotation
 $startButton.click(function()	{
 	Rotate();
+	//change label on button to 'stop' 
+    $button.attr("value","stop");
 });
 //define click function on clicking stop button
 $stopButton.click(function()	{
@@ -32,8 +40,6 @@ $stopButton.click(function()	{
 
 //////  define rotate function  ////////
 function Rotate() {
-  //change label on button to 'stop' 
-  $button.attr("value","stop");
   //select the image that is currently active (class == ‘top’) that we want to hide
   var $current = $rotator.find("img.top");
   //select the next sibling which we want to display
