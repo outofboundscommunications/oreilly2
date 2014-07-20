7320f865be53e849a662fbc527cf5d449fac306b
Jay Wilner <info@outofboundscommunications.com>
2014-07-20 13:52:59 -0500

    init commit of local changes before pull

--- lesson9/tabs.js
+++ lesson9/tabs.js
@@ -5,30 +5,38 @@ $(document).ready(function() {
     $("ul#tabs li a[href='blog']").parent().css("background-color", "#7ACC29");
     $("ul#tabs li a[href='contact']").parent().css("background-color", "#5C8A8A");
 
-    // hide all the content divs except the home content div
+    // hide all the tabs except the home tab
     $("div#content div").hide();
     $("div#content div#home").show();
+    $("ul#tabs li.selected").css("top", 20);
 
     // click handler for all tabs
-	$("ul#tabs li a").click(function(e) {
-		//var $link = $(this);
-		// prevent the a link from causing a page load
-		e.preventDefault();
-		// get the currently selected tab, and associated div
-		var selectedTabName = $("ul#tabs li.selected a").attr("href");
-		var $selectedDiv = $("div#" + selectedTabName);
-		// get the newly selected tab, and associated div
-		var tabName = $(this).attr("href");
-		var $div = $("div#" + tabName);
-		// hide the current content
-		$selectedDiv.hide("slow", function() {
-		// show the new content
-			console.log('hello');
-			$div.show("slow");
-			// update the currently selected tab
-			$("ul#tabs li.selected").removeClass("selected");
-			//$link.parent().addClass("selected");
-			$(this).parent().addClass("selected");
-		});
-	});
-});
+    $("ul#tabs li a").click(function(e) {
+        var $link = $(this);
+        // prevent the a link from causing a page load
+        e.preventDefault();
+        // get the currently selected tab, and associated div
+        var selectedTabName = $("ul#tabs li.selected a").attr("href");
+        var $selectedDiv = $("div#" + selectedTabName);
+        // get the newly selected tab, and associated div
+        var tabName = $(this).attr("href");
+        var $div = $("div#" + tabName);
+		console.log('the tab just clicked is: ' + tabName);
+        
+        $div.css("z-index", 1);
+        $div.slideDown("slow", function() {
+            $selectedDiv.hide();
+            $div.css("z-index", 0);
+        });
+
+       $("ul#tabs li.selected").animate({
+            top: 0
+        });
+        $("ul#tabs li.selected").removeClass("selected");
+
+        $(this).parent().animate({
+            top: 20
+        });
+        $(this).parent().addClass("selected");
+    });
+});  
