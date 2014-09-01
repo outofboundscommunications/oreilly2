aff17ed4b5d741db5e98bae1a3a9e942db552eb4
Jay Wilner <info@outofboundscommunications.com>
2014-09-01 18:20:34 -0500

    in progress partially working

--- Lesson11-Ajax/Obj1/todo1.js
+++ Lesson11-Ajax/Obj1/todo1.js
@@ -1,49 +1,47 @@
 
-//define array to hold the todo records we fetch from json data
-var todos = new Array();
-
-
+//define variable to hold the JSON data
 var data;
 
+//start main function ////////
 window.onload = init;
-//when ready, call function that gets the todo data from the json file
+
 function init() {
-    getMyToDoData();
+    //function that gets the todo data from the json file
+	getMyToDoData();
+	 $("#accordion").accordion({
+            collapsible: true,
+            header: "> div > ul#todoList > li"
+        }).sortable({
+            axis: "y",
+            handle: "h1"
+        });
+	
 }
 
+//end main function ////////
+
+//fetch JSON data
 function getMyToDoData () {
     $.get("todo.json", function(getTodoData) {
         console.log("Data loaded successfully");
         data = getTodoData;
         console.log(data);
+		//add todos to page
+		addTodosToPage(data);
+		$("#todoList li").addClass("ui-widget-content");
     }, "json");    
 }
 
+//add todo items to page
+function addTodosToPage(data) {
+    for (var i = 0; i <data.length; i++) {
+	//select the ul element we want to add <li>'s to
+	$("ul#todoList").each(function()	{
+		console.log($(this));
+		$(this).append("<li>" +data[i].who + " needs to " + data[i].task + " by " + data[i].dueDate);
+	});
+};
+	
+};
 
-function parseTodoItems(todoJSON) {
-    if (todoJSON == null || todoJSON.trim() == "") {
-        return;
-    }
-    var todoArray = JSON.parse(todoJSON);
-    if (todoArray.length == 0) {
-        console.log("Error: the to-do list array is empty!");
-        return;
-    }
-    for (var i = 0; i < todoArray.length; i++) {
-        var todoItem = todoArray[i];
-        todos.push(todoItem);
-    }
-    console.log("To-do array: ");
-    console.log(todos);
-}
-
-function addTodosToPage() {
-    var ul = document.getElementById("todoList");
-    for (var i = 0; i < todos.length; i++) {
-        var todoItem = todos[i];
-        var li = document.createElement("li");
-        li.innerHTML =
-            todoItem.who + " needs to " + todoItem.task + " by " + todoItem.dueDate;
-        ul.appendChild(li);
-    }
-}    
+   
