65dc64a976866efee770a58124bc9295611cddf4
Jay Wilner <info@outofboundscommunications.com>
2014-11-22 10:12:47 -0600

    initial commit before new branch attempt

--- Lesson12-Utils/Lesson12-Homework/eu.js
+++ Lesson12-Utils/Lesson12-Homework/eu.js
@@ -2,26 +2,29 @@
 
 $(document).ready(function() {
 
-//variable to hold json data
+//define variable to hold json data
 var data;
-//variable to hold slider selection
-var selection;
+//define variable to hold slider selection, this is the value you are going to multiply the data points
+//by to increase/decrease them with the slider movement
+var multiplier;
 
-//get json data
+//call function to get json data
 getEUJsonData();
-   
+
+//define slider function
 $(function() {
     $( "#slider-range-max" ).slider({
       range: "max",
       min: 1,
       max: 10,
+	  step: 1,
       value: 1,
       slide: function( event, ui ) {
         $( "#amount" ).val( ui.value );
 		//console.log(ui.value);
-		var selection = ui.value;
-		//update the data point size to the value user selected from slider
-		updateDataPoints(selection);
+		var multiplier = ui.value;
+		//call function that updates the data point size to the value user selected from slider
+		updateDataPoints(multiplier);
       }
     });
     $( "#amount" ).val( $( "#slider-range-max" ).slider( "value" ) );
@@ -30,20 +33,23 @@ $(function() {
 
 });
 
-function updateDataPoints(selection)	{
+function updateDataPoints(multiplier)	{
 	console.log(data);
-	console.log('update data points to this value:' + selection);
-	$(data).each(function(i)	{
-		console.log('the current data rate is: ' + data[i].rate);
-		console.log('the new data rate is: ' + parseFloat(data[i].rate)*parseFloat(selection));
+	console.log('update data points to this value:' + multiplier);
+	$("div.dataPt").each(function(i)	{
+		//console.log('the current data rate is: ' + data[i].rate);
+		//console.log('the new data rate is: ' + parseFloat(data[i].rate)*parseFloat(multiplier));
 		//replace current with new value
-		data[i].rate =parseFloat(data[i].rate)*parseFloat(selection);
+		
+		//data[i].rate =parseFloat(data[i].rate)*parseFloat(multiplier);
+		console.log('hello');
+		var theNewSize = parseFloat(data[i].rate)*parseFloat(multiplier/2);
+		$(this).css("borderRadius",theNewSize);
 	});
 	//now call function to replot these resized data points on map again
 	plotOnMap();
 	
 }
-
 
 //function to fetch JSON data and then display
 function getEUJsonData () {
@@ -62,26 +68,26 @@ function plotOnMap ()   {
         //create div
         //store country and rate in variables to use later
         var myCountry = data[i].country;
+		//store data.rate (unemployment rate) - it will be used as the width of the div (circle)
         var myRate = data[i].rate;
+		//to create a circle, use the border-radius propery and set equal to width of div/2
+		var myBorderRadius = myRate/2;
 		//create the div to add to the map as a circle
         $("<div " + "data-country=" + myCountry + " data-rate=" + myRate + "/>" + "</div>")
 		//select the map div, add the data point div and add the classes to style that data point
-        //.appendTo("div#map").addClass("dataPt").addClass("dataPtHover")
 		.appendTo("div#map").addClass("dataPt")
 		//add the hover event to: (1) display the data point details in the details box at top of page and
 		//(2) display the country name just to the side of the data point
-        .bind("hover", displayDetails)
+        .bind("hover", displayDetails,displayDetails)
 		//add the styling to the data point div that positions the data point on the correct location on the map
 		.css( {
             position:"absolute",
             left: data[i].x,
             top: data[i].y,
-            width: data[i].rate,
-            height: data[i].rate,
-            borderRadius: 50
+            width: myRate,
+            height: myRate,
+            borderRadius: myBorderRadius
         })
-		//add the hover event to display the country name just to the side of the data point
-       //.bind("mouseover", displayCountryName);
         
     })
 }
@@ -90,19 +96,25 @@ function displayDetails(evt)   {
 	//this function does two things:
 	//(1) display the data point details in the details box at top of page and
 	//(2) display the country name just to the side of the data point
-	$("div.dataPt").removeClass("dataPtHover");
-	$(this).addClass("dataPtHover");
-	//$("div.dataPt").addClass("dataPtHover");
-	//select the detail text box area at top of page left
-    //remove any text there already
-    $("#detail p").remove();
-	//now go ahead and add the currently active/hover country unemployment data in the detail box at top of page
-    $("#detail span").append("<p>"+ $(this).attr("data-rate") + "</p>");
 	
+	//remove all the 'dataPtHover' classes
+	$("div.dataPt").removeClass("dataPtHover");
+	//remove all the 'dataDetail' classes
+	$("p").removeClass("dataDetail");
 	//remove any country names already displayed as a paragraph next to a data point
 	$(".dataPt p").remove();
+	//select the detail text box area at top of page left, remove any text there already
+    $("#detail p").remove();
+	
+	//now for the active div being hovered upon, add that 'dataPtHover'class
+	$(this).addClass("dataPtHover");
 	//now go ahead and add the currently active/hover country name as text next to the data point
-    $(this).append("<p>"+ $(this).attr("data-country") + "</p>");
+    $(this).append("<p>"+ $(this).attr("data-country") + " (2011)" + "</p>");
+	$("div.dataPtHover p").addClass("dataDetail");
+	
+	//now go ahead and add the currently active/hover country unemployment data in the detail box at top of page
+    $("#detail span").append("<p>"+ $(this).attr("data-rate") + "</p>");
+
 
 }
                
